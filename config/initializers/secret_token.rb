5f2f8c1e9d4878d71591b3c12115e0c59b7a1f76
Matt Dziuban <mrdziuban@gmail.com>
2013-08-17 19:16:26 -0400

    First commit. Add devise for user authentication.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LifeManager::Application.config.secret_token = '124372c5f8f18ca23f109c65e1b44b0e731dce6a0916ba2566d7324154042c0ecd6f0b2844533e2f71eb5fc7f0834110be0b01088f7db538fa089bc73ca1c49c'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+LifeManager::Application.routes.draw do
+  devise_for :users
+
+  root to: "users#show"
+end
