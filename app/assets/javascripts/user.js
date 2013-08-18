f2118fdf8dac54fcbe8eaf68515893a6fe4acc29
Matt Dziuban <mrdziuban@gmail.com>
2013-08-17 21:34:16 -0400

    Add ability to add items on list creation and jQuery to add new items to form.

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -4,6 +4,12 @@ var User = function () {
   var init = function (myUserId) {
     userId = myUserId;
     $("#sidebar input[type=submit]").on("click", createList)
+
+    $(".add-item").on("click", function () {
+      var itemId = $("form > textarea").length
+      var html = "<label for='list_item_" + itemId + "_text'>Text</label><textarea name='list[items_attributes][" + itemId + "][text]'' id='list_item_" + itemId + "_text'></textarea><br>"
+      $("input[type=submit]").before($(html).hide().fadeIn("fast"));
+    });
   };
 
   var createList = function (event) {
