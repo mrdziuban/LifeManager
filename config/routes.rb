ce836f796098585cf9f46bfb3b32dbccccc5e4fb
Matt Dziuban <mrdziuban@gmail.com>
2013-08-17 20:11:11 -0400

    Add routes for lists and items.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 LifeManager::Application.routes.draw do
   devise_for :users, path: ""
 
+  resources :lists, only: [:create, :destroy]
+  resources :items, only: [:create]
+
   root to: "users#show"
 end
