92a3e01039284fa69a170cc3c586252b1e53c9fe
Matt Dziuban <mrdziuban@gmail.com>
2013-08-17 21:03:11 -0400

    Add JavaScript for user show page, including AJAX request for list creation.

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,34 @@
+var User = function () {
+  var userId;
+
+  var init = function (myUserId) {
+    userId = myUserId;
+    $("#sidebar input[type=submit]").on("click", createList)
+  };
+
+  var createList = function (event) {
+    var that = this;
+    event.preventDefault();
+
+    var formData = $(that.form).serialize();
+    formData += "&list%5Buser_id%5D=" + userId;
+    console.log(formData);
+
+    $.ajax({
+      url: that.form.action,
+      method: "POST",
+      data: formData,
+      success: function (data) {
+        $("#content .user-lists").append($(data).hide().fadeIn("fast"));
+        that.form.reset();
+      },
+      error: function () {
+        console.log("error");
+      }
+    });
+  };
+
+  return {
+    init: init
+  }
+}();
