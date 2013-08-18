640afb34b7aabcec3bb46b6b9e085a6b63c5d37f
Matt Dziuban <mrdziuban@gmail.com>
2013-08-17 22:12:26 -0400

    Add boolean column to items to indicate completion.

---
+++ db/migrate/20130818020947_add_completion_to_items.rb
@@ -0,0 +1,5 @@
+class AddCompletionToItems < ActiveRecord::Migration
+  def change
+    add_column :items, :completed, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130818000504) do
+ActiveRecord::Schema.define(:version => 20130818020947) do
 
   create_table "items", :force => true do |t|
     t.text     "text"
     t.integer  "list_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+    t.boolean  "completed",  :default => false
   end
 
   create_table "lists", :force => true do |t|
