60bb077dc0de5ebc0854048a97b6611e4f62d3c6
Matt Dziuban <mrdziuban@gmail.com>
2013-08-17 20:26:17 -0400

    Make list model accept nested attributes for items.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,9 +1,11 @@
 class List < ActiveRecord::Base
-  attr_accessible :title, :user_id
+  attr_accessible :title, :user_id, :items_attributes
   default_scope :include => :items
 
   validates :title, presence: true
 
   belongs_to :user
   has_many :items
+
+  accepts_nested_attributes_for :items, reject_if: :all_blank
 end
