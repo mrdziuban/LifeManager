c7695b576f80c096f6782461e27e811d9281266d
Matt Dziuban <mrdziuban@gmail.com>
2013-08-17 21:48:48 -0400

    Add expansion of items on click of list title.

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -10,6 +10,16 @@ var User = function () {
       var html = "<label for='list_item_" + itemId + "_text'>Text</label><textarea name='list[items_attributes][" + itemId + "][text]'' id='list_item_" + itemId + "_text'></textarea><br>"
       $("input[type=submit]").before($(html).hide().fadeIn("fast"));
     });
+
+    $(".user-lists > li").on("click", function () {
+      $(this).children(".list-items").slideToggle("fast");
+      if ($(this).find(".circle-plus").attr("src") == "/assets/circle_plus.png" || $(this).find(".circle-plus").attr("src") == "/assets/circle_plus-f0ee6d85e489c5b6de90ded216286da9.png") {
+        $(this).find(".circle-plus").attr("src", "/assets/circle_minus.png");
+      }
+      else {
+        $(this).find(".circle-plus").attr("src", "/assets/circle_plus.png");
+      }
+    })
   };
 
   var createList = function (event) {
